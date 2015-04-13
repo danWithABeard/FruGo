ee130d7e6a15f2192816f7b30feba6734ccd9890
Dan Cicconi <dcicconi@paperlesspost.com>
2015-04-13 00:28:14 -0400

    Adding new controller methods

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -3,4 +3,29 @@ class PackagesController < ApplicationController
   def index
   end
 
+  def show
+    @package = Package.find(params[:id])
+  end
+
+  def new
+    @package = Package.new
+  end
+
+  def create
+    length = params[:package][:length]
+    width = params[:package][:width]
+    height = params[:package][:height]
+    weight = params[:package][:weight]
+    
+    @package = Package.new(length: length, width: width, height: height, weight: weight)
+
+    # Save to the database
+    if @package.save
+      redirect_to @package
+    else
+      render "new"
+    end    
+  end
+
+
 end
