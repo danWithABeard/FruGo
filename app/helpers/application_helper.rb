8d67f07e6864971808c172f3cafc66037a29ab0c
Dan Cicconi <dcicconi@paperlesspost.com>
2015-04-19 10:32:09 -0400

    Adding usps xml builder method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	def build_usps_xml(package) 
+    usps_builder = Nokogiri::XML::Builder.new do |xml|
+      xml.package {
+        xml.height_ package.height
+        xml.length_ package.length
+        xml.width_ package.width
+        xml.weight_ package.weight
+      }
+    end
+    usps_builder.to_xml
+  end  
 end
