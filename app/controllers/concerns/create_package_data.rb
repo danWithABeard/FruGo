b7997849807513d7811e8816a96e8102ac5c53a8
Dan Cicconi <dcicconi@paperlesspost.com>
2015-04-22 20:26:38 -0400

    Moving xml function to packages_helper

--- app/controllers/concerns/create_package_data.rb
+++ app/controllers/concerns/create_package_data.rb
@@ -6,19 +6,4 @@ module CreatePackageData
     dimension_array.push(dim1, dim2, dim3)
     return dimension_array.sort!
   end
-
-	def build_usps_xml(package) 
-    usps_builder = Nokogiri::XML::Builder.new do |xml|
-      xml.package {
-        xml.height_ package.height
-        xml.length_ package.length
-        xml.width_ package.width
-        xml.girth_ package.girth
-        xml.weight_ package.weight
-        xml.zip_origination_ package.zip_origination
-        xml.zip_destination_ package.zip_destination
-      }
-    end
-    usps_builder.to_xml
-  end
 end
